5eb51ee819091bad1260c36abaffd201081fd3e8
Aragon <asd159874123@gmail.com>
2020-04-11 17:15:39 +0800

    V0.0

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # python_send2Phone
 使用python将消息推送到手机，目前支持Bark和Server酱
+
+# 依赖
+requests
+
+# 使用方法
+直接在Python中引用
---
+++ send2phone.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import requests
+
+barklink = "https://bark.test.com/bark_key"
+skey = "SCUxxxxxxxxxxxxx"
+def send2bark(title, content):
+    try:
+        msg = "{0}/推送标题/{1}/{2}".format(barklink, title, content)
+        link = msg
+        res = requests.get(link,verify=False)
+    except Exception as e:
+        print('Reason:', e)
+        return
+    return
+    
+def send2s(title, content):
+    try:
+        link = "https://sc.ftqq.com/{0}.send".format(skey)
+        d = {'text': title, 'desp': content}
+        res = requests.post(link, data=d , verify=False)
+    except Exception as e:
+        print('Reason:', e)
+        return
+    return
